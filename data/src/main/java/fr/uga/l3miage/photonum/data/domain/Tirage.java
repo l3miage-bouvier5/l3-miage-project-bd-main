2e524eef6d25bc2a3adb9b0f889e0109eaf8069c
l3miage-delagral <alex.del57640@gmail.com>
2023-03-13 18:21:53 +0100

    Impression et Heritages

---
+++ data/src/main/java/fr/uga/l3miage/photonum/data/domain/Tirage.java
@@ -0,0 +1,12 @@
+package fr.uga.l3miage.photonum.data.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.OneToMany;
+
+public class Tirage extends Impression{
+    
+    @OneToMany
+    private Set<Photo> photos = new HashSet<Photo>();
+}
