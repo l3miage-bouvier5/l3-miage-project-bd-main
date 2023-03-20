dd3d82b73b56f96058289cc6be9a96647f2043d5
l3miage-girylu <lucasgir8@gmail.com>
2023-03-20 17:21:54 +0100

    photocontroller

--- app/src/main/java/fr/uga/l3miage/photonum/photo/PhotoController.java
+++ app/src/main/java/fr/uga/l3miage/photonum/photo/PhotoController.java
@@ -1,5 +1,12 @@
 package fr.uga.l3miage.photonum.photo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1", produces = "application/json")
 public class PhotoController {
+
+    
     
 }
