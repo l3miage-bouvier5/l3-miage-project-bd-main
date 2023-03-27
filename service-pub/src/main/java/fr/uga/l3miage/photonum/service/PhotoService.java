dd6932be5c1c4c77f06f0c91efddc91971bc43a9
l3miage-delagral <alex.del57640@gmail.com>
2023-03-27 14:29:49 +0200

    oui

--- service-pub/src/main/java/fr/uga/l3miage/photonum/service/PhotoService.java
+++ service-pub/src/main/java/fr/uga/l3miage/photonum/service/PhotoService.java
@@ -1,13 +1,25 @@
 package fr.uga.l3miage.photonum.service;
 
 import java.util.Collection;
-
 import fr.uga.l3miage.photonum.data.domain.Photo;
 import fr.uga.l3miage.photonum.service.base.BaseService;
 
 public interface PhotoService extends BaseService<Photo, Long>{
+    
+    /**
+     * Saves a photo object
+     *
+     * @param photo to be saved
+     * @return the photo with an id
+     */
     Photo save(Photo photo);
 
-    Collection<Photo> SeachByName(String name);
+    /**
+     * Deletes a photo
+     *
+     * @param id id of the photo to delete
+     * @throws EntityNotFoundException when the entity do not already exists
+     */
+    void delete(Long id);
 
 }
